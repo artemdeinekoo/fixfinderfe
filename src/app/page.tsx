e9861601c760221bc5698c35a1884d61ecb6a9c3
Artem Deineko <artemdeinekoo@gmai.com>
2023-10-03 21:24:52 +0300

    added new h2 for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       <h1 className="p-40 hover:bg-sky-700 cursor-pointer">Артем Гей!</h1>
+      <h2 className="p-40 hover:bg-sky-700 cursor-pointer">Остап Гей!</h2>
     </main>
   );
 }
